e025ac3a0e403f89cedead9b32462f827e810b16
bhanutejaswini <rbhanutejaswini@gmail.com>
2024-08-22 16:10:59 +0530

    Add UI

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
+const buttons = document.querySelectorAll("button");
+const headingEle = document.querySelector("h3");
+const humanChoiceEle = document.querySelector(".human-choice");
+const humanScoreEle = document.querySelector(".human-score");
+const computerChoiceEle = document.querySelector(".computer-choice");
+const computerScoreEle = document.querySelector(".computer-score");
+
+let humanScore = 0,
+  computerScore = 0;
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
 
   const randomIndex = Math.floor(Math.random() * choices.length);
 
   return choices[randomIndex];
-}
-
-function getHumanChoice() {
-  let humanChoice;
-  const validChoices = ["rock", "paper", "scissors"];
-
-  do {
-    humanChoice = prompt("Your choice: ").toLowerCase();
-
-    if (!validChoices.includes(humanChoice))
-      alert("Your choice is invalid! Enter rock, paper or scissors");
-  } while (!validChoices.includes(humanChoice));
-
-  return humanChoice;
 }
 
 function playRound(computerChoice, humanChoice) {
@@ -36,41 +32,52 @@ function playRound(computerChoice, humanChoice) {
   }
 }
 
-function playGame() {
-  const rounds = 5;
-  let humanScore = 0,
-    computerScore = 0;
+function resetGame() {
+  humanScore = 0;
+  computerScore = 0;
+  humanChoiceEle.textContent = "";
+  computerChoiceEle.textContent = "";
+  humanScoreEle.textContent = "Player: 0";
+  computerScoreEle.textContent = "Computer: 0";
+  headingEle.textContent = "First player to score 5 points wins!!";
+}
 
-  for (let i = 0; i < rounds; i++) {
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-    console.log(computerChoice, humanChoice);
+function playGame(humanChoice) {
+  let computerChoice = getComputerChoice();
 
-    const result = playRound(computerChoice, humanChoice);
+  const result = playRound(computerChoice, humanChoice);
 
-    humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1);
-    computerChoice = computerChoice[0].toUpperCase() + computerChoice.slice(1);
+  humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1);
+  computerChoice = computerChoice[0].toUpperCase() + computerChoice.slice(1);
 
-    if (result === 1) {
-      computerScore++;
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-    } else if (result === 2) {
-      humanScore++;
-      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-    } else {
-      console.log(`Its is a draw! You and computer selected ${humanChoice}`);
-    }
+  humanChoiceEle.textContent = humanChoice;
+  computerChoiceEle.textContent = computerChoice;
+
+  if (result === 1) {
+    computerScore++;
+
+    computerScoreEle.textContent = `Computer: ${computerScore}`;
+    headingEle.textContent = `${humanChoice} is beaten by ${computerChoice}`;
+  } else if (result === 2) {
+    humanScore++;
+
+    humanScoreEle.textContent = `Player: ${humanScore}`;
+    headingEle.textContent = `${humanChoice} beats ${computerChoice}`;
+  } else {
+    headingEle.textContent = `You and computer selected ${humanChoice}`;
   }
 
-  console.log(`Final Score: Computer ${computerScore} - You ${humanScore}`);
-
-  if (computerScore > humanScore) {
-    console.log(`You lose!`);
-  } else if (computerScore < humanScore) {
-    console.log(`You win!`);
-  } else {
-    console.log(`Its is a draw!`);
+  if (computerScore === 5) {
+    headingEle.textContent = `You lose! Game will be reset.`;
+    setTimeout(resetGame, 5000);
+  } else if (humanScore === 5) {
+    headingEle.textContent = `You win! Game will be reset.`;
+    setTimeout(resetGame, 5000);
   }
 }
 
-playGame();
+for (const button of buttons) {
+  button.addEventListener("click", (e) => {
+    playGame(e.target.id);
+  });
+}
